088bcf55c3158b82c5becae0d6499012b445e946
Igor henrique <igorhenrriquealmeida@hotmail.com>
2025-05-08 13:03:28 -0300

    criação do pacote "_003_operadores" e da classe "Operadores.java"

---
+++ Java-basico/meu primeiro projeto java/src/edu/igor/sintaxe_basica_com_java/aprendendo_a_sintaxe_java/_003_operadores/Operadores.java
@@ -0,0 +1,9 @@
+package edu.igor.sintaxe_basica_com_java.aprendendo_a_sintaxe_java._003_operadores;
+
+public class Operadores {
+    public static void main(String[] args) {
+        String nome1 = "Igor";
+        String nome2 = "Igor";
+        System.out.println(nome1==nome2);
+    }
+}
