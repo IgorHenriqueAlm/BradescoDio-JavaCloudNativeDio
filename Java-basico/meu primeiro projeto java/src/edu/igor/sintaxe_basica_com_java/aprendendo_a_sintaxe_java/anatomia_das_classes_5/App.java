1b322f8d410bd8ebea2a96365c5ef4ac9855d26e
Igor henrique <igorhenrriquealmeida@hotmail.com>
2025-05-08 10:48:28 -0300

    add pastas

---
+++ Java-basico/meu primeiro projeto java/src/edu/igor/sintaxe_basica_com_java/aprendendo_a_sintaxe_java/anatomia_das_classes_5/App.java
@@ -0,0 +1,13 @@
+package edu.igor.sintaxe_basica_com_java.aprendendo_a_sintaxe_java.anatomia_das_classes_5;
+public class App {
+    public static void main(String[] args) throws Exception {
+        String priNome = "Igor";
+        String segNome = "Henrique";
+        String complNome = nomeCompleto(priNome, segNome);
+        System.out.println(complNome);
+    }
+    
+    public static String nomeCompleto (String primNome , String segNome){
+        return primNome.concat(" ").concat(segNome);
+    }
+}
