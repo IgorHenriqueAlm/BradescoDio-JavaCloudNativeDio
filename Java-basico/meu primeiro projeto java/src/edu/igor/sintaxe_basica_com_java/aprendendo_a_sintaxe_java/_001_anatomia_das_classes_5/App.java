2ee4158b04d07cf00a036fc2875c0a6af124f058
Igor henrique <igorhenrriquealmeida@hotmail.com>
2025-05-08 11:39:36 -0300

    alteração no mone do pacote "_001_anatomia_das_classes_5" e criação de novo pacote "_002_tipos_de_variaveis"

--- Java-basico/meu primeiro projeto java/src/edu/igor/sintaxe_basica_com_java/aprendendo_a_sintaxe_java/_001_anatomia_das_classes_5/App.java
+++ Java-basico/meu primeiro projeto java/src/edu/igor/sintaxe_basica_com_java/aprendendo_a_sintaxe_java/_001_anatomia_das_classes_5/App.java
@@ -1,4 +1,4 @@
-package edu.igor.sintaxe_basica_com_java.aprendendo_a_sintaxe_java.anatomia_das_classes_5;
+package edu.igor.sintaxe_basica_com_java.aprendendo_a_sintaxe_java._001_anatomia_das_classes_5;
 public class App {
     public static void main(String[] args) throws Exception {
         String priNome = "Igor";
---
+++ Java-basico/meu primeiro projeto java/src/edu/igor/sintaxe_basica_com_java/aprendendo_a_sintaxe_java/_002_tipos_de_variaveis/TiposVariaveis.java
@@ -0,0 +1,6 @@
+package edu.igor.sintaxe_basica_com_java.aprendendo_a_sintaxe_java._002_tipos_de_variaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+    }
+}
