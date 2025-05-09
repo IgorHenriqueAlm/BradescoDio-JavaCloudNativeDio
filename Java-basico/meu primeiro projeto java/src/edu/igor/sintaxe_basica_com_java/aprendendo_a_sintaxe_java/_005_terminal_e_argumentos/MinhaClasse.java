dc9901bceae16559ba8c81ce50125e2ffa713190
Igor henrique <igorhenrriquealmeida@hotmail.com>
2025-05-09 15:41:59 -0300

    criei o pacote "_005_terminal_e_argumentos" e a classe "MinhaClasse.java"

---
+++ Java-basico/meu primeiro projeto java/src/edu/igor/sintaxe_basica_com_java/aprendendo_a_sintaxe_java/_005_terminal_e_argumentos/MinhaClasse.java
@@ -0,0 +1,7 @@
+package edu.igor.sintaxe_basica_com_java.aprendendo_a_sintaxe_java._005_terminal_e_argumentos;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Oi,fui executatdo pelo terminal.");
+    }
+}
