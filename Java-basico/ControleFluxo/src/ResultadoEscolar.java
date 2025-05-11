6a785e48ff216d62512bc23581561f8d27d9ddb4
Igor henrique <igorhenrriquealmeida@hotmail.com>
2025-05-11 15:21:34 -0300

    Update ResultadoEscolar.java

--- Java-basico/ControleFluxo/src/ResultadoEscolar.java
+++ Java-basico/ControleFluxo/src/ResultadoEscolar.java
@@ -9,5 +9,12 @@ public class ResultadoEscolar {
         }else {
             System.out.println("Reprovado");
         }
+        
+        //////////////////////////////////////
+        //Refatotrando para OPERADOR TERNÁRIO
+
+        String resultado = nota >= 7 ? "Aprovado" :nota >= 5 && nota < 7 ? "Recuperação" : "Reporvado";
+
+        System.out.println(resultado);
     }
 }
