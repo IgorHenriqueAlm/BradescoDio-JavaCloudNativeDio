55b65d93c2c22ab18ef6f2cbe410aeae98c80705
Igor henrique <igorhenrriquealmeida@hotmail.com>
2025-05-11 15:06:49 -0300

    Update ResultadoEscolar.java

--- Java-basico/ControleFluxo/src/ResultadoEscolar.java
+++ Java-basico/ControleFluxo/src/ResultadoEscolar.java
@@ -1,10 +1,12 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 10;
+        int nota = 6;
 
         if (nota>=7) {
             System.out.println("Aprovado");
-        } else {
+        }else if (nota >= 5 && nota < 7) {
+            System.out.println("Prova de recuperação");
+        }else {
             System.out.println("Reprovado");
         }
     }
