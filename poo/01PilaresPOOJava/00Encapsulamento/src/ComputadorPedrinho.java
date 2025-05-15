15bd5e7b4f9b63fb6b9aa074257c0b924196c6d9
Igor henrique <igorhenrriquealmeida@hotmail.com>
2025-05-15 16:53:16 -0300

    Create ComputadorPedrinho.java

---
+++ poo/01PilaresPOOJava/00Encapsulamento/src/ComputadorPedrinho.java
@@ -0,0 +1,8 @@
+public class ComputadorPedrinho {
+    public static void main(String[] args) {
+        MSNmensseger msn = new MSNmensseger();
+
+        msn.enviarMenssagem();
+        msn.recberMenssagem();
+    }
+}
