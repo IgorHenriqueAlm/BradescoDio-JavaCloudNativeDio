5c1b6e0f6a59cc000f4da884bb17909d2fce3494
Igor henrique <igorhenrriquealmeida@hotmail.com>
2025-05-15 23:55:15 -0300

    recriado proj de emcapsulamento

---
+++ poo/01PilaresPOOJava/00Encapsulamento/00Encapsulamento/src/com/igor/app/ComputadorPedrinho.java
@@ -0,0 +1,10 @@
+package com.igor.app;
+
+public class ComputadorPedrinho {
+    public static void main(String[] args) {
+        MSNmensseger msn = new MSNmensseger();
+
+        msn.enviarMenssagem();
+        msn.recberMenssagem();
+    }
+}
---
+++ poo/01PilaresPOOJava/00Encapsulamento/00Encapsulamento/src/com/igor/app/MSNmensseger.java
@@ -0,0 +1,18 @@
+package com.igor.app;
+
+public class MSNmensseger {
+    public void enviarMenssagem(){
+        validarConexaoInternet();
+        System.out.println("Enviando menssagem");
+        salvarHistoricoMenssagem();
+    }
+    public void recberMenssagem(){
+        System.out.println("Recbendo menssagem");
+    }
+    private void validarConexaoInternet(){
+        System.out.println("Validando se há conexão à internet");
+    }
+    private void salvarHistoricoMenssagem(){
+        System.out.println("Salvando histórico da mensagem");
+    }
+}
