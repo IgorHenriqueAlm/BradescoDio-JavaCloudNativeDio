e7c164fa9066466cc231941ee2b8bd779ed6812a
Igor henrique <igorhenrriquealmeida@hotmail.com>
2025-05-25 14:30:31 -0300

    Update Banco.java

--- 01_poo/Criando_um_Banco_Digital_com_Java_e_Orientação_a_Objetos/Banco_digital/src/Banco.java
+++ 01_poo/Criando_um_Banco_Digital_com_Java_e_Orientação_a_Objetos/Banco_digital/src/Banco.java
@@ -1,24 +1,34 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Banco {
+    private String nome;
+    private List<Conta> contas;
 
-	private String nome;
-	private List<Conta> contas;
+    public Banco(String nome) {
+        this.nome = nome;
+        this.contas = new ArrayList<>();
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public void adicionarConta(Conta conta) {
+        contas.add(conta);
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public List<Conta> getContas() {
+        return contas;
+    }
 
-	public List<Conta> getContas() {
-		return contas;
-	}
+    public Conta buscarContaPorNumero(int numero) {
+        return contas.stream().filter(c -> c.numero == numero).findFirst().orElse(null);
+    }
 
-	public void setContas(List<Conta> contas) {
-		this.contas = contas;
-	}
-
+    public List<Conta> buscarContasPorCliente(String nomeCliente) {
+        List<Conta> resultado = new ArrayList<>();
+        for (Conta conta : contas) {
+            if (conta.cliente.getNome().equalsIgnoreCase(nomeCliente)) {
+                resultado.add(conta);
+            }
+        }
+        return resultado;
+    }
 }
