514dfa913f7b1ea936710dbfd8723a5ba706fa66
Igor henrique <igorhenrriquealmeida@hotmail.com>
2025-05-25 14:29:32 -0300

    Update ContaPoupanca.java

--- 01_poo/Criando_um_Banco_Digital_com_Java_e_Orientação_a_Objetos/Banco_digital/src/ContaPoupanca.java
+++ 01_poo/Criando_um_Banco_Digital_com_Java_e_Orientação_a_Objetos/Banco_digital/src/ContaPoupanca.java
@@ -1,12 +1,14 @@
 public class ContaPoupanca extends Conta {
 
-	public ContaPoupanca(Cliente cliente) {
-		super(cliente);
-	}
+    private static final double RENDIMENTO = 0.01; // 1%
 
-	@Override
-	public void imprimirExtrato() {
-		System.out.println("=== Extrato Conta Poupan�a ===");
-		super.imprimirInfosComuns();
-	}
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
+
+    public void aplicarRendimento() {
+        double ganho = saldo * RENDIMENTO;
+        depositar(ganho);
+        historico.add("Rendimento aplicado: R$ " + ganho);
+    }
 }
