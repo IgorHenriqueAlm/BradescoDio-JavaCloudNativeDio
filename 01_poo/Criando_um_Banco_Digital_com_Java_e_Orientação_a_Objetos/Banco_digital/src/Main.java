e1a52a8d1104994c59908899f4bb4c6c3cf60de0
Igor henrique <igorhenrriquealmeida@hotmail.com>
2025-05-25 14:31:22 -0300

    Update Main.java

--- 01_poo/Criando_um_Banco_Digital_com_Java_e_Orientação_a_Objetos/Banco_digital/src/Main.java
+++ 01_poo/Criando_um_Banco_Digital_com_Java_e_Orientação_a_Objetos/Banco_digital/src/Main.java
@@ -1,17 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        Cliente c1 = new Cliente("João", "123.456.789-00", "Rua A", "9999-0000");
+        Conta cc = new ContaCorrente(c1);
+        Conta cp = new ContaPoupanca(c1);
 
-	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+        cc.depositar(100);
+        cc.transferir(50, cp);
+        ((ContaPoupanca) cp).aplicarRendimento();
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
-	}
-
+        cc.imprimirExtratoDetalhado();
+        cp.imprimirExtratoDetalhado();
+    }
 }
