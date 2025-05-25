4f91707e1851b1c25ea6b0d4a63e3743db640b54
Igor henrique <igorhenrriquealmeida@hotmail.com>
2025-05-25 14:25:16 -0300

    Update Cliente.java

--- 01_poo/Criando_um_Banco_Digital_com_Java_e_Orientação_a_Objetos/Banco_digital/src/Cliente.java
+++ 01_poo/Criando_um_Banco_Digital_com_Java_e_Orientação_a_Objetos/Banco_digital/src/Cliente.java
@@ -1,13 +1,50 @@
 public class Cliente {
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String telefone;
 
-	private String nome;
+    public Cliente(String nome, String cpf, String endereco, String telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void atualizarDados(String endereco, String telefone) {
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
 }
