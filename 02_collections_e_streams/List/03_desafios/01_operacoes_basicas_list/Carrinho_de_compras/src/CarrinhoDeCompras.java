b1f8e116867be2d06b4d6af70815f28ed71b0d84
igor_henrique <igorhenrriquealmeida@hotmail.com>
2026-03-16 16:33:20 -0300

    Resolução do desafio com operações básicas com List

---
+++ 02_collections_e_streams/List/03_desafios/01_operacoes_basicas_list/Carrinho_de_compras/src/CarrinhoDeCompras.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+    private List<Item> itemList;
+
+    public CarrinhoDeCompras() {
+        this.itemList = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade) {
+        this.itemList.add(new Item(nome, preco, quantidade));
+    }
+
+    public void removerItem(String nome){
+        List<Item> itensParaRemover = new ArrayList<>();
+        if (!itemList.isEmpty()) {
+            for (Item i : itemList) {
+                if (i.getNome().equalsIgnoreCase(nome)) {
+                    itensParaRemover.add(i);
+                }
+            }    
+            itemList.removeAll(itensParaRemover);
+        }else {
+            System.out.println("O carrinho está vazio.");
+        }
+    } 
+
+    public Double calcularValorTotal() {
+        double valorTotal = 0d;
+        if (!itemList.isEmpty()) {
+            for (Item item : itemList) {
+                double valorDoItem = item.getPreco() * item.getQuantidade();
+                valorTotal += valorDoItem;
+            }
+            return valorTotal;
+        }else {
+            throw new RuntimeException("O carrinho está vazio.");
+        }
+    }
+
+    public void exibirItens() {
+        if (!itemList.isEmpty()) {
+            System.out.println(this.itemList);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CarrinhoDeCompras{" +
+            "itens=" + itemList +
+            '}';
+    }
+
+}
